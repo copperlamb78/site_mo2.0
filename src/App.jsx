3997406127d9ee790248f06943c316fc54ca46d2
copperlamb78 <nascimentogabriel.2004@gmail.com>
2024-05-05 20:34:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Introduction } from './components/introdução'
+import { Fotosandvideos } from './components/fotosandvideos'
+
+
+
+import './global.css'
+import { VideoFinal } from './videofinal'
+export function App() {
+
+  return (
+    <div>
+      <Introduction />
+      <Fotosandvideos />
+      <VideoFinal />
+    </div>
+  )
+}
